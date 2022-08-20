a50340c0384a45d0cd74f460a0e5de14a77fc5d2
Elsaadany427 <hamadabassem427@gmail.com>
2022-08-20 11:56:51 +0200

    Added heading title for courses

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@ import "./App.css";
 import React from "react";
 import Navbar from "./components/Navbar/Navbar";
 import Header from "./components/Header/Header";
+import Courses from "./components/Courses/Courses";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <Header />
+      <div className="container">
+        <Header />
+
+        <Courses />
+      </div>
     </div>
   );
 }
---
+++ src/components/Courses/Courses.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Courses() {
+  return (
+    
+    // Main content -- Heading title
+    <div class="Head-title">
+      <h1>A broad selection of courses</h1>
+      <p>
+        Choose from 185,000 online video courses with new additions
+        published every month
+      </p>
+    </div>
+
+  )
+}
