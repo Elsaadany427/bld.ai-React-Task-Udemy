05fc81ebcc8fbf6aa02d2e4927ea98346ca5963d
Elsaadany427 <hamadabassem427@gmail.com>
2022-09-01 20:41:42 +0200

    Created pages files

--- src/App.js
+++ src/App.js
@@ -4,22 +4,11 @@ import "./App.css";
 import React from "react";
 // Components
 import Navbar from "./components/Navbar/Navbar";
-import Header from "./components/Header/Header";
-import Courses from "./components/Courses/Courses";
-import Categories from "./components/Categories/Categories";
-
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <div className="container">
-        <Header />
-        <Courses>
-        </Courses>
-        <Categories>
-        </Categories>
-      </div>
     </div>
   );
 }
