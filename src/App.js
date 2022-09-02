077080521289e801ec136ca13d640ce9795771ac
Elsaadany427 <hamadabassem427@gmail.com>
2022-09-02 14:35:48 +0200

    Added Header

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import "./App.css";
 import React from "react";
 // Components
 import Navbar from "./components/Navbar/Navbar";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Home />
     </div>
   );
 }
