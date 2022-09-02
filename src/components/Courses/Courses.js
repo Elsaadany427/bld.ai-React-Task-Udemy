8d6d891045aa34dfe1c826571fac2625ae4d9944
Elsaadany427 <hamadabassem427@gmail.com>
2022-09-02 18:05:09 +0200

    Added tabs and need some edit

--- src/components/Courses/Courses.js
+++ src/components/Courses/Courses.js
@@ -1,11 +1,25 @@
 import React from "react";
+import { useState, useEffect } from "react";
 import Card from "../Card/Card";
 import { CardDate } from "../../Database/CardData";
 
 export default function Courses(props) {
+  const [Tap, setTap] = useState([]);
+
+  useEffect(() => {
+    fetch("https://my-json-server.typicode.com/Sara-Samer/udemy/tabs")
+      .then((data) => data.json())
+      .then((data) => {
+        setTap(data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
   return (
     <>
-      {/* Main content -- Heading title */}
+      {/* Heading title */}
       <div className="Head-title">
         <h1>A broad selection of courses</h1>
         <p>
@@ -13,8 +27,19 @@ export default function Courses(props) {
           every month
         </p>
       </div>
+      <div className="cources-tab">
+        <nav className="cources-nav">
+          <ul className="cources-list">
+            {Object.keys(Tap).map((element, index) => (
+              <li className={`cources-item ${index === 0 && "active"}`} key={element}>
+                <a href={`#${element}`}>{element}</a>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      </div>
       <div className="container courses">
-        <div className="row">
+        <div className="row" style={{ clear: "both" }}>
           {CardDate.map((card) => (
             <Card key={card.id} CardData={card} />
           ))}
