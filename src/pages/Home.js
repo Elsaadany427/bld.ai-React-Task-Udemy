13b888a54847422f9d850642fd5224a1ed658004
Elsaadany427 <hamadabassem427@gmail.com>
2022-09-02 14:51:11 +0200

    Added Caregory component to home file

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "../components/Header/Header";
 import Courses from "../components/Courses/Courses";
+import Categories from "../components/Categories/Categories";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <div className="container">
         <Header />
         <Courses />
+        <Categories />
       </div>
     </>
   );
