6de5a4926d7908412b8f8e67255733b099630cbe
Elsaadany427 <hamadabassem427@gmail.com>
2022-09-03 19:36:33 +0200

    Added tabs without default tab work

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,37 +1,32 @@
 import React from "react";
 
 export default function Card(props) {
-  const {image, title, auther, starRate, students, price} =  props.CardData;
+  const { image, title, auther, starRate, students, price } = props.CardData;
   return (
-    <div className="col-12 col-sm-6 col-md-4 col-lg-3">
-      <div className="course-card">
-        <div className="card-img">
-          <img
-            src={`/courses/${image}`}
-            alt={image}
-          />
-        </div>
-        <h3>
-          <a href="#h"> { title } </a>
-        </h3>
-        <div>
-          <span> {auther} </span>
-        </div>
-        <div className="card-stars-container">
-          <span>
-            <span> {starRate} </span>
-            <span className="card-stars">
-              <i className="fa fa-star"></i>
-              <i className="fa fa-star"></i>
-              <i className="fa fa-star"></i>
-              <i className="fa fa-star-half-o"></i>
-            </span>
-            <span>({students})</span>
+    <div className="course-card">
+      <div className="card-img">
+        <img src={`/courses${image}`} alt={image} />
+      </div>
+      <h3>
+        <a href="#h"> {title} </a>
+      </h3>
+      <div>
+        <span> {auther} </span>
+      </div>
+      <div className="card-stars-container">
+        <span>
+          <span> {starRate} </span>
+          <span className="card-stars">
+            <i className="fa fa-star"></i>
+            <i className="fa fa-star"></i>
+            <i className="fa fa-star"></i>
+            <i className="fa fa-star-half-o"></i>
           </span>
-        </div>
-        <div className="card-price">
-          <span> E£{price} </span>
-        </div>
+          <span>({students})</span>
+        </span>
+      </div>
+      <div className="card-price">
+        <span> E£{price} </span>
       </div>
     </div>
   );
--- src/components/Courses/Courses.js
+++ src/components/Courses/Courses.js
@@ -1,21 +1,53 @@
 import React from "react";
 import { useState, useEffect } from "react";
 import Card from "../Card/Card";
-import { CardDate } from "../../Database/CardData";
+
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/swiper.min.css";
+
+// import required modules
+import { Navigation, Pagination, Mousewheel, Keyboard } from "swiper";
 
 export default function Courses(props) {
-  const [Tap, setTap] = useState([]);
+  const [Tabs, setTabs] = useState([]);
+  const [currentTab, setcurrentTab] = useState({
+    tabName: "",
+    opportunities: "",
+    desc: "",
+  });
+  const [courses, setCourses] = useState([]);
 
   useEffect(() => {
-    fetch("https://my-json-server.typicode.com/Sara-Samer/udemy/tabs")
+    // Fetch tabs
+    fetch("https://62f965f63eab3503d1e45e85.mockapi.io/tabs")
       .then((data) => data.json())
       .then((data) => {
-        setTap(data);
+        setTabs(data[0]);
+       
       })
       .catch((err) => {
         console.log(err);
       });
   }, []);
+
+  function handleCourses(tab) {
+    fetch(`https://62f965f63eab3503d1e45e85.mockapi.io/courses?category=${tab}`)
+      .then((data) => data.json())
+      .then((data) => {
+        setcurrentTab({
+          tabName: tab,
+          opportunities: Tabs[tab].opportunities,
+          desc: Tabs[tab].desc,
+        });
+        setCourses(data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
 
   return (
     <>
@@ -30,19 +62,62 @@ export default function Courses(props) {
       <div className="cources-tab">
         <nav className="cources-nav">
           <ul className="cources-list">
-            {Object.keys(Tap).map((element, index) => (
-              <li className={`cources-item ${index === 0 && "active"}`} key={element}>
-                <a href={`#${element}`}>{element}</a>
+            {Object.keys(Tabs).map((tab, index) => (
+              <li
+                className={`cources-item ${index === 0 && "active"}`}
+                key={tab}
+                onClick={() => handleCourses(tab) }
+              >
+                <a href={`#${tab}`}>{tab}</a>
               </li>
             ))}
           </ul>
         </nav>
       </div>
       <div className="container courses">
+        <div className="main-content__course-desc">
+          <h2> {currentTab.opportunities} </h2>
+          <p>{currentTab.desc}</p>
+          <button className="btn btn-secondry btn-height">
+            Explore {currentTab.tabName}
+          </button>
+        </div>
+
         <div className="row" style={{ clear: "both" }}>
-          {CardDate.map((card) => (
-            <Card key={card.id} CardData={card} />
-          ))}
+          <Swiper
+            navigation={true}
+            mousewheel={true}
+            keyboard={true}
+            modules={[Navigation, Pagination, Mousewheel, Keyboard]}
+            spaceBetween={20}
+            slidesPerView={1}
+            pagination={{
+              clickable: true,
+            }}
+            // modules={[Pagination]}
+            breakpoints={{
+              // when window width is >= 640px
+              640: {
+                width: 240,
+                slidesPerView: 1,
+              },
+              // when window width is >= 768px
+              768: {
+                width: 568,
+                slidesPerView: 2,
+              },
+            }}
+            id="main"
+            width="280"
+          >
+            {/* <div class="swiper-wrapper"> */}
+            {courses.map((card) => (
+              <SwiperSlide key={card.id}>
+                <Card key={card.id} CardData={card} />
+              </SwiperSlide>
+            ))}
+            {/* </div> */}
+          </Swiper>
         </div>
       </div>
     </>
