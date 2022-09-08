106c827396791572fa3837e369511bfc2743658f
Elsaadany427 <hamadabassem427@gmail.com>
2022-09-08 14:46:54 +0200

    Added Course header

---
+++ src/pages/Course/Course.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from './components/Header'
+import Learn from './components/Learn'
+
+export default function Course() {
+  return (
+    <>
+    <section className='course'>
+      <Header />
+      <Learn />
+      </section>
+    </>
+  )
+}
---
+++ src/pages/Course/components/Learn.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./Learn.css";
+
+export default function Learn() {
+  return (
+    <div className="container learn">
+      <h2>What you'll learn</h2>
+      <ul>
+        <li>
+          <i class="fa fa-check" aria-hidden="true"></i>
+          <span>You will master professional Python programming</span>
+        </li>
+        <li>
+          <i class="fa fa-check" aria-hidden="true"></i>
+          <span>You will master professional Python programming</span>
+        </li>
+        <li>
+          <i class="fa fa-check" aria-hidden="true"></i>
+          <span>You will master professional Python programming</span>
+        </li>
+      </ul>
+    </div>
+  );
+}
